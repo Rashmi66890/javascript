e9a2d58dba9e334084437c653366d06ce07d14e2
Rashmi <rashmideep21@gmail.com>
2022-07-25 11:28:12 +0530

    first commit

---
+++ bulb/ras.js
@@ -0,0 +1,18 @@
+let myFunction =(a,b) =>
+{
+console.log(a+b)
+}
+myFunction(4,7);
+let num=10
+for(let i=0; i<=9 ;i++)
+{
+    console.log(i)
+}
+const data = document.getElementById("demo")
+console.log(data)
+data.style.backgroundColor="red"
+data.style.textAlign="center"
+
+for (let i = 0; i < 5; i++) {
+    document.getElementById(`div${i}`).style.backgroundColor = "white";
+  }
---
+++ script.js
@@ -0,0 +1,102 @@
+function bulbToggle() {
+  let bulb = document.getElementById("lightbulb");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle1() {
+  let bulb = document.getElementById("lightbulb1");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle2() {
+  let bulb = document.getElementById("lightbulb2");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle3() {
+  let bulb = document.getElementById("lightbulb3");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle4() {
+  let bulb = document.getElementById("lightbulb4");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle5() {
+  let bulb = document.getElementById("lightbulb5");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle6() {
+  let bulb = document.getElementById("lightbulb6");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle7() {
+  let bulb = document.getElementById("lightbulb7");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle8() {
+  let bulb = document.getElementById("lightbulb8");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }
+}
+function bulbToggle9() {
+  let bulb = document.getElementById("lightbulb9");
+
+  if (bulb.src.match("bulboff")) {
+    bulb.src = "bulbon.png";
+  } else {
+    bulb.src = "bulboff.jpeg";
+  }}
+function turnAllBulbOn(){
+  bulbToggle();
+  bulbToggle1();
+  bulbToggle2();
+  bulbToggle3();
+  bulbToggle4();
+  bulbToggle5();
+  bulbToggle6();
+  bulbToggle7();
+  bulbToggle8();
+  bulbToggle9();
+  
+}
